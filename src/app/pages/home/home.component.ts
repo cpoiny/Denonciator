1415c18e8a700fdd725adf6b341d174f17228d7c
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-02-19 19:12:31 +0100

    [feat/service]
    Creation du service tirage au sort mais ne fonctionne pas pour le moment
    Création de la fonction tirage au sort et affichage... en cours

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  
+  listeDesEtudiants : string[] = ['Fama', 'Cynthia', 'Anne', 'Julie'];
 
+  // créer une liste qui recupére les selectionné
+  listeDesSelectionné : string[] = [];
+  nom! : string
+  
+  // Créer une fonction qui permet de choisir aléatoirement
+  tirageAuSort() {
+    const choix = Math.floor(Math.random()*this.listeDesEtudiants.length);
+    const nom = this.listeDesEtudiants[choix];
+   console.log(nom);
+   this.nom = nom;
+  }
 }
--- src/app/pages/tirage/tirage.component.ts
+++ src/app/pages/tirage/tirage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student, STUDENTSLIST } from 'src/app/mocks/students.mock';
+import { RandomService } from 'src/app/services/random.service';
 
 @Component({
   selector: 'app-tirage',
@@ -7,5 +9,36 @@ import { Component } from '@angular/core';
 })
 export class TirageComponent {
 
+//   // Liste des etudiants dont le statut est présent à créer
+//   listOfStudentsPresent! : Student[];
+//   listTest! : Student [];
 
+//   // Etudiant à créer
+//  // student : Student = new Student: any();
+
+// constructor(private service : RandomService) {}
+
+// //Ajout d'un étudiant dans le tableau
+// addStudent() {
+//  // this.service.createStudent(this.student);
+//   //this.student = new Student();
+// }
+
+
+listOfStudent: Student[] = STUDENTSLIST;
+listeDesEtudiants : string[] = ['Fama', 'Cynthia', 'Anne', 'Julie'];
+
+// créer une liste qui recupére les selectionné
+listeDesSelectionné : string[] = [];
+nom! : string;
+selection! :string;
+
+// Créer une fonction qui permet de choisir aléatoirement
+tirageAuSort() {
+  const choix = Math.floor(Math.random()*this.listOfStudent.length);
+  const nom = this.listOfStudent[choix];
+  const selection = nom.identity;
+ console.log(nom);
+ this.selection = selection;
+}
 }
