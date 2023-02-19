e856911f81302bd75757461d92a9ccce106d7aae
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-02-19 01:12:26 +0100

    [feat/absences] Liste des absents par genre

--- src/app/pages/absent/absent.component.ts
+++ src/app/pages/absent/absent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student, STUDENTSLIST } from 'src/app/mocks/students.mock';
 
 @Component({
   selector: 'app-absent',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./absent.component.css']
 })
 export class AbsentComponent {
-
+  listOfStudent : Student[] = STUDENTSLIST;
+  displayOnlyAbsent : boolean = false;
+  
+  
+  changeStatus(){
+    if(this.displayOnlyAbsent){
+   this.displayOnlyAbsent = false;
+    }else {
+    this.displayOnlyAbsent = true;
+  }
+  console.log(this.displayOnlyAbsent);
+  }
 }
---
+++ src/app/pages/tirage/tirage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tirage',
+  templateUrl: './tirage.component.html',
+  styleUrls: ['./tirage.component.css']
+})
+export class TirageComponent {
+
+
+}
