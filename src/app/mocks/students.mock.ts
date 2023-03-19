2e852f528b82deceec0928cde77dd3210e996ee9
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-19 08:31:44 +0100

    ajout boolean canBeSelected dans le mock

--- src/app/mocks/students.mock.ts
+++ src/app/mocks/students.mock.ts
@@ -4,6 +4,7 @@ export interface Student{
     identity:string;
     gender:Gender;
     status:boolean;
+    canBeSelected: boolean;
     
 }
 export const STUDENTSLIST :Student[]=[
@@ -11,109 +12,127 @@ export const STUDENTSLIST :Student[]=[
         id:1,
         identity: "Fahd",
         gender: 'Homme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:2,
         identity: "Jerome",
         gender: 'Homme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:3,
         identity: "Thomas",
         gender: 'Homme',
-        status: false
+        status: false,
+        canBeSelected: true
     },
     {
         id:4,
         identity: "Erdal",
         gender: 'Homme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:5,
         identity: "Hazar",
         gender: 'Femme',
-        status: false
+        status: false,
+        canBeSelected: true
     },
     {
         id:6,
         identity: "Clara",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:7,
         identity: "Diambere",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:8,
         identity: "Christelle",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:9,
         identity: "Liliana",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:10,
         identity: "Kenzia",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:11,
         identity: "Fama",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:12,
         identity: "Morgane",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:13,
         identity: "Jiyun",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:14,
         identity: "Anne",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:15,
         identity: "Assa",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:16,
         identity: "Julie",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:17,
         identity: "Katia",
         gender: 'Femme',
-        status: true
+        status: true,
+        canBeSelected: true
     },
     {
         id:18,
         identity: "Cynthia",
         gender: 'Femme',
-        status: false
+        status: false,
+        canBeSelected: true
     }
 
 ]; 
--- src/app/pages/tirage/tirage.component.ts
+++ src/app/pages/tirage/tirage.component.ts
@@ -26,41 +26,38 @@ export class TirageComponent {
 
 
   listOfStudent: Student[] = STUDENTSLIST;
-   
+
   selection!: string;
   result!: string;
 
 
-  ngOnInit(){
+  ngOnInit() {
     this.tirageAuSort();
   }
   // Créer une fonction qui permet de choisir aléatoirement
   tirageAuSort() {
 
     // je recupere un index
-   // while (this.listOfStudent.length > 0) {
+    // while (this.listOfStudent.length > 0) {
     const index = Math.floor(Math.random() * this.listOfStudent.length);
     // je recupere la valeur de l'index
     const nom = this.listOfStudent[index];
     // j'exclu les absents
     if (nom.status) {
       // j'affiche le message selon le genre
-    if (nom.gender === "Homme") {
-      this.result = "Tu es le grand gagnant!";
+      if (nom.gender === "Homme") {
+        this.result = "Tu es le grand gagnant!";
+      } else {
+        this.result = "Tu es la grande gagnante!";
+      }
+      // je recupere l'attribut identity de l'etudiant
+      const selection = nom.identity;
+      // j'assigne a ma variable selection la valeur de l'etudiant selectionne
+      this.selection = selection;
     } else {
-      this.result = "Tu es la grande gagnante!";
+      //je retire le nom de la liste
+      this.tirageAuSort();
     }
-    // je recupere l'attribut identity de l'etudiant
-    const selection = nom.identity;
-    // j'assigne a ma variable selection la valeur de l'etudiant selectionne
-    this.selection = selection;
-  } else {
-    //je retire le nom de la liste
-    this.tirageAuSort();
-  }
-   //this.listOfStudent.splice(index, 1, nom);
-   // console.log("nouvelle liste" , this.listOfStudent);
-    //console.log("length", this.listOfStudent);
-  //  }
+    
   }
 }
